5c0d2794071b4b45f151d2d6fb39659dd2c00671
Artem Laptiev <artuon@gmail.com>
2021-11-01 22:08:14 +0200

    See local for DB default

--- src/users/entities/user.ts
+++ src/users/entities/user.ts
@@ -1,11 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger'
 
 import { IUser } from '../interfaces/user.interface'
+import { Column, Index, PrimaryGeneratedColumn } from 'typeorm'
 
 export class User implements Omit<IUser, 'password'> {
   @ApiProperty({ type: String })
+  @PrimaryGeneratedColumn()
   id: string
 
   @ApiProperty({ type: String })
+  @Column()
+  @Index({ unique: true })
   username: string
 }
