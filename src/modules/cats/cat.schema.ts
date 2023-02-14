86e60c7c217c1e6eb480e280b77eb4d101e3cc16
Artu <artuon@gmail.com>
2023-02-14 11:51:30 +0200

    Update README.md

--- src/modules/cats/cat.schema.ts
+++ src/modules/cats/cat.schema.ts
@@ -1,18 +1,18 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
 
-export type CatDocument = HydratedDocument<Cat>;
+export type CatDocument = HydratedDocument<Cat>
 
 @Schema()
 export class Cat {
   @Prop()
-  name: string;
+  name: string
 
   @Prop()
-  age: number;
+  age: number
 
   @Prop()
-  breed: string;
+  breed: string
 }
 
-export const CatSchema = SchemaFactory.createForClass(Cat);
+export const CatSchema = SchemaFactory.createForClass(Cat)
