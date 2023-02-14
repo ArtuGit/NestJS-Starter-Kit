201049ec54f2711d9331211c61ace3be3b1ac618
Artu <artuon@gmail.com>
2023-02-14 19:17:55 +0200

    MongoExceptionFilter

--- src/modules/contacts/contacts.controller.ts
+++ src/modules/contacts/contacts.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common'
+import { Controller, Get, Post, Delete, Body, Param, HttpCode, HttpStatus, UseFilters } from '@nestjs/common'
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 
 import { ValidateMongoId } from '../../common/validations/mongo-id.validation'
+import { MongoExceptionFilter } from '../../common/exceptions/mongo-exception.filter'
 
 import { ContactsService } from './contacts.service'
 import { Contact } from './contact.schema'
 import { CreateContactBody } from './dto/create-contact.body'
 
+@UseFilters(MongoExceptionFilter)
 @ApiTags('Contact')
 @Controller({
   path: 'contact',
