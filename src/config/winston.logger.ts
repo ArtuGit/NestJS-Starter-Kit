3fd63525685bd1721102120d7373d55a6e034f96
Artu <artuon@gmail.com>
2024-06-16 10:19:58 +0200

    Change logger injection

--- src/config/winston.logger.ts
+++ src/config/winston.logger.ts
@@ -1,43 +1,6 @@
-import { Injectable } from '@nestjs/common'
-import { transports, createLogger, format, Logger } from 'winston'
 import * as winston from 'winston'
 import { WinstonModule, utilities as nestWinstonUtilities } from 'nest-winston'
 import { envConfig } from './env.config'
-
-// const errorStackTracerFormat = format((info) => {
-//   if (info.stack) {
-//     info.message = `${info.message}\n> ${info.stack}`
-//     delete info.stack
-//   }
-//   return info
-// })
-//
-// @Injectable()
-// export class WinstonLogger {
-//   private logger: Logger
-//
-//   constructor() {
-//     this.logger = createLogger({
-//       level: 'info',
-//       format: format.combine(format.timestamp(), format.json()),
-//       transports: [
-//         new transports.Console({
-//           format: format.combine(errorStackTracerFormat(), format.colorize(), format.simple()),
-//         }),
-//         new transports.File({ filename: 'error.log', level: 'error' }),
-//         new transports.File({ filename: 'combined.log' }),
-//       ],
-//     })
-//   }
-//
-//   log(message: string) {
-//     this.logger.log('info', message)
-//   }
-//
-//   error(message: string) {
-//     this.logger.error(message)
-//   }
-// }
 
 export const getWinstonLoggerModule = () => {
   const level = envConfig.LOGGING_LEVEL
