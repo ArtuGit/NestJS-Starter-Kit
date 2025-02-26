0affc17a1582d108fb89f7b252922716715f3991
Artu <artuon@gmail.com>
2025-02-26 21:39:02 +0100

    Swagger Auth

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,5 +4,15 @@ import * as packageJson from '../../package.json'
 export const swaggerConfig = new DocumentBuilder()
   .setTitle(packageJson.name)
   .setVersion(packageJson.version)
-  .addBearerAuth()
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'JWT-auth', // This name must match the decorator parameter
+  )
   .build()
